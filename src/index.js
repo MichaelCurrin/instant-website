a67ff1f90a2d146488cea371bf7ec253baf3a0c7
Michael Currin <michael.currin@gmail.com>
2020-03-18 20:47:10 +0200

    feat: Add src folder

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configureUrlQuery } from 'react-url-query';
+
+import App from './App';
+import history from './history';
+
+// Link the history used in our app to url-query so it can update the URL with it.
+configureUrlQuery({ history });
+
+ReactDOM.render(<App />, document.getElementById('root'));
