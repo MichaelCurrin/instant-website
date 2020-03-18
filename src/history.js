6b1ad582b86b7ec2de13035061d9161e1ae8c3a7
Michael Currin <michael.currin@gmail.com>
2020-03-18 20:55:51 +0200

    docs: Add comment

--- src/history.js
+++ src/history.js
@@ -1,3 +1,5 @@
+// Dependency warning says to use: require("history").createBrowserHistory
+// But doesn't explain how to do this for import.`
 import createHistory from 'history/createBrowserHistory';
 
 const history = createHistory();
