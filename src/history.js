1ceee5df38a5950e1d4593e5c4a10a5f8473137d
Michael Currin <michael.currin@gmail.com>
2020-03-29 20:25:17 +0200

    fix: Remove deprecation warning

--- src/history.js
+++ src/history.js
@@ -1,6 +1,4 @@
-// Dependency warning says to use: require("history").createBrowserHistory
-// But doesn't explain how to do this for import.`
-import createHistory from 'history/createBrowserHistory';
+const createHistory = require('history').createBrowserHistory;
 
 const history = createHistory();
 
