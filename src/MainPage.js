ae537eb029141f2840a1aad872191ec68e00d7a9
Michael Currin <michael.currin@gmail.com>
2020-03-29 12:32:54 +0200

    feat: Add form card

--- src/MainPage.js
+++ src/MainPage.js
@@ -69,16 +69,109 @@ function MainCard(props) {
     );
 }
 
+function FormCard(props) {
+    return (
+        <div id="controls">
+            <h3 class="subtitle">Customize this page</h3>
+
+            <div class="field">
+                <label class="label" for="title-input">
+                    Title
+                </label>
+                <div class="control">
+                    <input
+                        class="input"
+                        type="text"
+                        id="title-input"
+                        name="title-input"
+                        placeholder="Your title"
+                        size="22"
+                        maxlength="22"
+                    />
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label" for="subtitle-input">
+                    Subtitle
+                </label>
+                <div class="control">
+                    <input
+                        class="input"
+                        id="subtitle-input"
+                        name="subtitle-input"
+                        type="text"
+                        placeholder="Your subtitle"
+                        size="35"
+                        maxlength="35"
+                    />
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label" for="description-input">
+                    Description
+                </label>
+                <div class="control">
+                    <textarea
+                        class="textarea"
+                        id="description-input"
+                        name="description-input"
+                        placeholder="Enter your longer description"
+                        rows="3"
+                        cols="50"
+                    />
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label" for="main-image-input">
+                    Main image
+                </label>
+                <div class="control">
+                    <div class="select" id="main-image-input" name="main-image-input">
+                        <select>
+                            <option value="">(none)</option>
+                            <option>african</option>
+                            <option>picture</option>
+                            <option>software</option>
+                            <option>sock</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label" for="bg-image-input">
+                    Background image
+                </label>
+                <div class="control">
+                    <div class="select" id="bg-image-input" name="bg-image-input">
+                        <select>
+                            <option value="">(none)</option>
+                            <option>african</option>
+                            <option>picture</option>
+                            <option>software</option>
+                            <option>sock</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 function Modal(props) {
-    const { cardBody } = props;
+    const { displayCard, formCard } = props;
 
     return (
         <div class="modal is-active">
             <div class="modal-card">
                 <header class="modal-card-head" />
-
-                <section class="modal-card-body">{cardBody}</section>
-
+                <section class="modal-card-body">
+                    <div>{displayCard}</div>
+                    <div>{formCard}</div>
+                </section>
                 <footer class="modal-card-foot" />
             </div>
         </div>
@@ -144,16 +237,17 @@ class MainPage extends PureComponent {
             url: mainImageUrl
         };
 
-        const cardBody = MainCard({
+        const displayCard = MainCard({
             title: title,
             subtitle: subtitle,
             description: description,
             mainImageValues: mainImageValues
         });
+        const formCard = FormCard({});
 
         return (
             <div>
-                <Modal cardBody={cardBody} />
+                <Modal displayCard={displayCard} formCard={formCard} />
                 <section
                     id="bg"
                     class="hero is-fullheight"
