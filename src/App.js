e868db191e89b5a34a80a0b65c122fa9aa4c903f
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-16 14:48:51 +0530

    Add basic multi-page-website layout - Add routes for all pages - Add navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import MainPage from './MainPage';
 import history from './history';
 
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Home from './Components/home/Home';
+import About from './Components/about/About';
+import Showcase from './Components/showcase/Showcase';
+import Custom from './Components/custom/Custom';
+import Terms from './Components/terms/Terms';
+import Header from './Components/Header';
+
 class App extends Component {
     componentDidMount() {
         // Force an update if the URL changes.
@@ -10,9 +18,17 @@ class App extends Component {
 
     render() {
         return (
-            <main role="main">
-                <MainPage />
-            </main>
+          <Router>
+            <Header />
+            <Switch>
+              <Route exact path='/'><Redirect to='/home' /></Route>
+              <Route path='/home'><Home /></Route>
+              <Route path='/custom'><Custom /></Route>
+              <Route path='/showcase'><Showcase /></Route>
+              <Route path='/about'><About /></Route>
+              <Route path='/terms'><Terms /></Route>
+            </Switch>
+          </Router>
         );
     }
 }
