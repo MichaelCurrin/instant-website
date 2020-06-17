ac28b8357e02820e62954f4c361ef70185e95cd5
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-17 15:17:53 +0530

    Add custom website feature - Add Edit component which contains the form for custom website - Add View component which displays the custom website - Configure url updates by using the useQueryParam hook in Custom component   and passing the functions supplied by the hook to update params to Edit   and view components.
    The custom component checks if there are any query parameters supplied
    to it and makes necessary changes in the redux state.
    This process happens once. (see useEffect hook in custom component)
    
    All the input controls in the Edit component trigger query and redux state
    changes.
    mainImage and bgImage query params are now based on ArrayParam type
    supplied by useQueryParam.
    The conversion of comma separated string to query params is done in
    customWebsiteDataSlice.js

--- src/Components/custom/Custom.js
+++ src/Components/custom/Custom.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQueryParam, NumberParam, StringParam, ArrayParam, withDefault } from 'use-query-params';
+import View from './View';
+import Edit from './Edit';
+import { useSelector, useDispatch } from 'react-redux';
+import { isDataBeingEditedUpdated, titleUpdated, subtitleUpdated, descriptionUpdated, mainImageKeywordsChanged, bgImageKeywordsChanged } from '../../app/customWebsiteDataSlice';
 
 const Custom = () => {
+  const edit = useSelector(state => state.customWebsiteData.isDataBeingEdited)
+  const bgImageUrl = useSelector(state => state.customWebsiteData.bgImageUrl)
+
+  const [editParam, setEditParam] = useQueryParam('edit', NumberParam)
+  const [titleParam, setTitle] = useQueryParam('title', StringParam)
+  const [subtitleParam, setSubtitle] = useQueryParam('subtitle', StringParam)
+  const [descriptionParam, setDescription] = useQueryParam('description', StringParam)
+  const [mainImageParam, setMainImage] = useQueryParam('mainImage', withDefault(ArrayParam, []))
+  const [bgImageParam, setBgImage] = useQueryParam('bgImage', withDefault(ArrayParam, []))
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if(typeof editParam === 'number') dispatch(isDataBeingEditedUpdated(editParam == 1 ? true : false))    
+    if(titleParam) dispatch(titleUpdated(titleParam))
+    if(subtitleParam) dispatch(subtitleUpdated(subtitleParam))
+    if(descriptionParam) dispatch(descriptionUpdated(descriptionParam))
+    if(mainImageParam.length > 0) dispatch(mainImageKeywordsChanged(mainImageParam.join(',')))
+    if(bgImageParam.length > 0) dispatch(bgImageKeywordsChanged(bgImageParam.join(',')))
+
+    if(typeof editParam === 'number') console.log('redux state updated from editParam')    
+    if(titleParam) console.log('redux state updated from titleParam')
+    if(subtitleParam) console.log('redux state updated from subtitleParam')
+    if(descriptionParam) console.log('redux state updated from descriptionParam')
+    if(mainImageParam.length > 0) console.log('redux state updated from mainImageParam')
+    if(bgImageParam.length > 0) console.log('redux state updated from bgImageParam')
+  }, [])
+
+  console.log(bgImageUrl)
   return (
-    <div>
-      Custom
+    <div style={{ width: '100%', height: '100%', position: 'absolute', overflow: 'auto', backgroundImage: `url(${bgImageUrl})` }}>
+      <div className='container' style={{ margin: '2rem 10rem' }} >
+        {edit === false ? (
+          <View changeEditParam={setEditParam} /> 
+        ) : (
+          <Edit 
+            changeEditParam={setEditParam}
+            changeTitleParam={setTitle}
+            changeSubtitleParam={setSubtitle}
+            changeDescriptionParam={setDescription}
+            changeMainImageParam={setMainImage}
+            changeBgImageParam={setBgImage} />
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/Components/custom/View.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { MAIN_IMG_W, MAIN_IMG_H, REPO_URL, PROJECT_TITLE, MAIN_PAGE_DISPLAY_DEFAULTS } from '../../constants';
+import { isDataBeingEditedUpdated } from '../../app/customWebsiteDataSlice';
+import { useSelector, useDispatch } from 'react-redux';
+
+const View = props => {
+  
+  const title = useSelector(state => state.customWebsiteData.title)
+  const subtitle = useSelector(state => state.customWebsiteData.subtitle)
+  const description = useSelector(state => state.customWebsiteData.description)
+  const mainImageUrl = useSelector(state => state.customWebsiteData.mainImageUrl)
+  const bgImageUrl = useSelector(state => state.customWebsiteData.bgImageUrl)
+
+  // This flag can be changed later when some image URLs are deterministic.
+  const randomImages = true;
+  const refreshMsg = randomImages ? ' - Refresh this page to pick random images.' : '';
+
+  const dispatch = useDispatch()
+
+  const onEditPageButtonClick = () => {
+    props.changeEditParam(1)
+    dispatch(isDataBeingEditedUpdated(true))
+  }
+
+  return (
+      <div className="card">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className={`image is-${MAIN_IMG_W}x${MAIN_IMG_H}`}>
+                <img className="is-rounded" src={mainImageUrl} alt="Small profile" />
+              </figure>
+            </div>
+
+            <div className="media-content">
+              <p className="title is-4">{title || MAIN_PAGE_DISPLAY_DEFAULTS.title}</p>
+              <p className="subtitle is-6">{subtitle || MAIN_PAGE_DISPLAY_DEFAULTS.subtitle}</p>
+            </div>
+          </div>
+
+          <p className="content">{description || MAIN_PAGE_DISPLAY_DEFAULTS.description}</p>
+          <br />
+
+          <footer className="card-footer">
+            <p className="card-footer-item">
+              <span>
+                Made with <a href={REPO_URL}>{PROJECT_TITLE}</a>
+                {refreshMsg}
+                <button className='button' onClick={onEditPageButtonClick}>Edit this website</button>
+              </span>
+            </p>
+          </footer>
+        </div>
+      </div>
+  );
+};
+
+export default View;
