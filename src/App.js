ad8fb5575f7003d3d6776073153e52c396a4f5d9
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-17 15:16:07 +0530

    Add some style changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-import MainPage from './MainPage';
-import history from './history';
-
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import React from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Home from './Components/home/Home';
 import About from './Components/about/About';
 import Showcase from './Components/showcase/Showcase';
@@ -10,27 +7,23 @@ import Custom from './Components/custom/Custom';
 import Terms from './Components/terms/Terms';
 import Header from './Components/Header';
 
-class App extends Component {
-    componentDidMount() {
-        // Force an update if the URL changes.
-        history.listen(() => this.forceUpdate());
-    }
-
-    render() {
-        return (
-          <Router>
-            <Header />
-            <Switch>
-              <Route exact path='/'><Redirect to='/home' /></Route>
-              <Route path='/home'><Home /></Route>
-              <Route path='/custom'><Custom /></Route>
-              <Route path='/showcase'><Showcase /></Route>
-              <Route path='/about'><About /></Route>
-              <Route path='/terms'><Terms /></Route>
-            </Switch>
-          </Router>
-        );
-    }
+const App = () => {
+  return (
+    <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <Header />
+      <div style={{ flexGrow: 1, position: 'relative' }}>
+        <Switch>
+          <Route exact path='/'><Redirect to='/home' /></Route>
+          <Route path='/home'><Home /></Route>
+          <Route path='/custom'><Custom /></Route>
+          <Route path='/showcase'><Showcase /></Route>
+          <Route path='/about'><About /></Route>
+          <Route path='/terms'><Terms /></Route>
+        </Switch>
+      </div>
+      
+    </div>
+  );
 }
 
 export default App;
