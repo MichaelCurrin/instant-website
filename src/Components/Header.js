b065a7181bdd0706a93b6dfd60f439ae9dc4ca52
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-17 15:25:40 +0530

    Change class to className in Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,24 +3,24 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <Link class="navbar-item" to="/">Instant Website</Link>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">Instant Website</Link>
 
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          <Link class="navbar-item" to='/home'>Home</Link>
-          <Link class="navbar-item" to='/custom' >Custom</Link>
-          <Link class="navbar-item" to='/showcase' >Showcase</Link>
-          <Link class="navbar-item" to='/about' >About</Link>
-          <Link class="navbar-item" to='/terms' >Terms</Link>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <Link className="navbar-item" to='/home'>Home</Link>
+          <Link className="navbar-item" to='/custom?edit=0' >Custom</Link>
+          <Link className="navbar-item" to='/showcase' >Showcase</Link>
+          <Link className="navbar-item" to='/about' >About</Link>
+          <Link className="navbar-item" to='/terms' >Terms</Link>
         </div>
       </div>
     </nav>
