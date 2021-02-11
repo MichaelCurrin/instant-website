8341045b40b81ddbdfef992c527ba82e2085935f
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-11 15:07:13 +0200

    revert

--- src/history.js
+++ src/history.js
@@ -1,4 +1,6 @@
 import { createBrowserHistory } from "history";
 
 // TODO Does this need to be an instance here? Why not just instatiated where needed?
-export default history = createBrowserHistory();
+const history = createBrowserHistory();
+
+export default history
