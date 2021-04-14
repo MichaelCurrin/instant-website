476d6c62b540a22377254540c8e047c336ffb250
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-14 10:49:15 +0200

    style

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,7 @@ import React from "react";
 /**
  * Modal component
  *
- * The formCard property is optional. The divider will only be rendered if it is set.
+ * The formCard property is optional. The divider will only be rendered if formCard is set.
  */
 function Modal(props) {
   const { displayCard, formCard } = props;
@@ -12,11 +12,15 @@ function Modal(props) {
     <div className="modal is-active">
       <div className="modal-card">
         <header className="modal-card-head" />
+
         <section className="modal-card-body">
           <div>{displayCard}</div>
+
           {formCard && <hr />}
+
           <div>{formCard}</div>
         </section>
+
         <footer className="modal-card-foot" />
       </div>
     </div>
